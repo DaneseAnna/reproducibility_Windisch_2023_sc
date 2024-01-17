3b8c46fee03d32e513f44c15148f5947449b2929
DrAlexFischer <156894661+DrAlexFischer@users.noreply.github.com>
2024-01-17 15:54:41 +0100

    Add files via upload
    Intital commit for bulk-RNAseq Analysis

---
+++ RNAseq_Code/RNAseq_MLL_ENL.mapping.sh
@@ -0,0 +1,11 @@
+mapRNAseq.sh -g GRCh38 -l 100 -f /misc/data/rawData/collaborations/bzkf/wichmann/p171_E3_DD-MLL-ENL_plus_shield1_S1_R1_001.fastq.gz -r /misc/data/rawData/collaborations/bzkf/wichmann/p171_E3_DD-MLL-ENL_plus_shield1_S1_R2_001.fastq.gz -d collaborations/bzkf/wichmann -n RNAseq_DDMLLENL_E3_plusShield1_p171 -t 12 -c 100 ;
+ 
+mapRNAseq.sh -g GRCh38 -l 100 -f /misc/data/rawData/collaborations/bzkf/wichmann/p171_E4_DD-MLL-ENL_plus_shield1_S2_R1_001.fastq.gz -r /misc/data/rawData/collaborations/bzkf/wichmann/p171_E4_DD-MLL-ENL_plus_shield1_S2_R2_001.fastq.gz -d collaborations/bzkf/wichmann -n RNAseq_DDMLLENL_E4_plusShield1_p171 -t 12 -c 100 ;
+ 
+mapRNAseq.sh -g GRCh38 -l 100 -f /misc/data/rawData/collaborations/bzkf/wichmann/p171_E7_DD-MLL-ENL_plus_shield1_S3_R1_001.fastq.gz -r /misc/data/rawData/collaborations/bzkf/wichmann/p171_E7_DD-MLL-ENL_plus_shield1_S3_R2_001.fastq.gz -d collaborations/bzkf/wichmann -n RNAseq_DDMLLENL_E7_plusShield1_p171 -t 12 -c 100 ;
+ 
+mapRNAseq.sh -g GRCh38 -l 100 -f /misc/data/rawData/collaborations/bzkf/wichmann/p171_E3_DD-MLL-ENL-shield1_differentated_S4_R1_001.fastq.gz -r /misc/data/rawData/collaborations/bzkf/wichmann/p171_E3_DD-MLL-ENL-shield1_differentated_S4_R2_001.fastq.gz -d collaborations/bzkf/wichmann -n RNAseq_DDMLLENL_E3_noShield1_differentated_p171 -t 12 -c 100 ;
+ 
+mapRNAseq.sh -g GRCh38 -l 100 -f /misc/data/rawData/collaborations/bzkf/wichmann/p171_E4_DD-MLL-ENL-shield1_differentated_S5_R1_001.fastq.gz -r /misc/data/rawData/collaborations/bzkf/wichmann/p171_E4_DD-MLL-ENL-shield1_differentated_S5_R2_001.fastq.gz -d collaborations/bzkf/wichmann -n RNAseq_DDMLLENL_E4_noShield1_differentated_p171 -t 12 -c 100 ;
+ 
+mapRNAseq.sh -g GRCh38 -l 100 -f /misc/data/rawData/collaborations/bzkf/wichmann/p171_E7_DD-MLL-ENL-shield1_differentated_S6_R1_001.fastq.gz -r /misc/data/rawData/collaborations/bzkf/wichmann/p171_E7_DD-MLL-ENL-shield1_differentated_S6_R2_001.fastq.gz -d collaborations/bzkf/wichmann -n RNAseq_DDMLLENL_E7_noShield1_differentated_p171 -t 12 -c 100 ; 
